779760b03046098159ef6e24bc3b4344723a7f40
dhouel <dhouel.lins@gmail.com>
2024-10-01 09:16:46 -0300

    classes, variaveis e costantes

---
+++ src/MyClass.java
@@ -0,0 +1,16 @@
+public class MyClass{
+    //nome de projeto todo em minusculo, palavras separadas por traço
+    //nome de classe sem traço ou _, palavras iniciando em maiuscula
+    public static void main(String[] args) {
+        System.out.print("Hello Word");
+        //metodo main, obrigatorio.
+        String primeiraVariavel="valor";//variavel se inicia em minuscula 
+        //porem por convenção a segunda palavra deve se iniciar 
+        //em maiuscula. Variaveis em maiuscula, por convenção são constantes
+        //pode-se definir a constante para identificação do java da seguinte forma
+        //nome deve conter letras, Underline, $ ou numeros
+        final String BR="Brasil";
+
+
+    }
+}
